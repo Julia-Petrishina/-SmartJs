355e5434b6471615d3b7e10ab275e3ee2eba30b0
julia <julia.petrishina@teamdev.com>
2015-12-01 15:03:43 +0200

    Replace code from test file to tha 10.1 file. Delete file test.js

--- 10/10.1.js
+++ 10/10.1.js
@@ -4,77 +4,59 @@ var list = {
         value: 2,
         next: {
             value: 3,
-            next: null
-
-        }
-    }
-};
-
-//var list = { add: function () {...}, foo: function () {...} }
-
-function add(list, v){
-
-
-    if(list.next === null){
-        list.next = {value: v, next: null};
-    }
-    else{
-        add(list.next, v);
-    }
-
-    return list;
-
-}
-
-
-
-
-var list = {value: 1, next: {value: 2, next: {value: 3, next: {value: 4, next: {value: 5, next: null}}}}};
-
-function get(list, index){
-
-    if(index === 0){
-        return list;
-    }
-    else{
-        var count = 1;
-        var currentList = list.next;
-        while(count < index){
-            currentList = currentList.next;
-            count++;
-
-            if(currentList === null){
-                return false;
+            next: {
+                value: 4,
+                next: {
+                    value: 5,
+                    next: null
+                }
             }
         }
+    },
 
-        return currentList;
+
+    get: function (list, index) {
+
+        if (index === 0) {
+            return list;
+        }
+        else {
+            var count = 1;
+            var currentList = list.next;
+            while (count < index) {
+                currentList = currentList.next;
+                count++;
+
+                if (currentList === null) {
+                    return false;
+                }
+            }
+
+            return currentList;
+        }
+    },
+
+    remove: function (list, index) {
+
+        var toBeRemovedList = list.get(list, index);
+        var nextToBeRemovedList = toBeRemovedList.next;
+        var curList = list.next;
+
+        var count = 0;
+        while(count < index--){
+            var prevList = curList;
+            curList = curList.next;
+            count++;
+        }
+
+        prevList.next = nextToBeRemovedList;
+        return toBeRemovedList;
+
     }
 
-}
+};
 
-
-function remove(list, index){
-    return get(list, index);
-    var toBeRemovedList = get(list, index);
-
-    var nextList = toBeRemovedList.next;
-    var curList = list.next;
-    var prevList = null;
-
-    while(!toBeRemovedList){
-        prevList = curList;
-        curList = curList.next;
-    }
-
-    toBeRemovedList = curList;
-    prevList.next = nextList;
-
-    return toBeRemovedList;
-
-}
-
-remove(list, 1)
-
+console.log(list.remove(list, 3));
 console.log(list);
+
 
