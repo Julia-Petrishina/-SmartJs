107b566fe2d93f794a81527880bc5746ed784c90
julia petrishina <j.petrishina@pulsarfour.com>
2015-12-20 20:32:48 +0200

    Add validation evaluation form

--- validation/index.js
+++ validation/index.js
@@ -1,37 +1,179 @@
-window.onload = function(){
+/**
+ * Created by Julia Petrishina on 12/16/15.
+ */
 
-    var tab1 = document.querySelector('.tab1');
-    var tab2 = document.querySelector('.tab2');
+(function ($) {
+    "use strict";
 
-    var form1 = document.querySelector('#myForm1');
-    var form2 = document.querySelector('#myForm2');
+    var validators = {
+        'required': function (rule, value) {
+            return '' !== value;
+        }
 
+        /*  'type': {
 
-    function tabs1() {
-        form1.style.display = 'block';
-        form2.style.display = "none";
+         'email': function (value) {
+         return value.match(/^(?:[\w\!\#\$\%\&\'\*\+\-\/\=\?\^\`\{\|\}\~]+\.)*[\w\!\#\$\%\&\'\*\+\-\/\=\?\^\`\{\|\}\~]+@(?:(?:(?:[a-zA-Z0-9](?:[a-zA-Z0-9\-](?!\.)){0,61}[a-zA-Z0-9]?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9\-](?!$)){0,61}[a-zA-Z0-9]?)|(?:\[(?:(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\.){3}(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\]))$/);
+         },
+
+         'number': function(value){
+         return value.match(/^([\+]+)*[0-9\x20\x28\x29\-]{5,20}$/);
+         },
+
+         'select': function(element){
+         return true;
+         },
+
+         'checkbox': function(element){
+         return true;
+         },
+
+         'radio': function(element){
+         return true;
+         }
+         }*/
+    };
+
+    var validationRules = {
+        'name': {
+            required: {
+                value: true,
+                message: "Name is required"
+            }
+        },
+
+        'email': {
+            required: {
+                value: true,
+                message: 'Email is required'
+            },
+            type: 'email'
+        },
+
+        'project-name': {
+            required: {
+                value: true,
+                message: "Project Name is required"
+            }
+        },
+
+        'source-code': {
+            required: {
+                value: true,
+                message: "Source code is required"
+            }
+        }
+    };
+
+    function getValue(element){
+        var value = element.val();
+        return value;
     }
 
-    function tabs2(){
-        form2.style.display = 'block';
-        form1.style.display = 'none';
+
+    function validate(formID) {
+        var formFields = $(formID + ' .required');
+        var errors = {};
+
+        for (var index = 0; index < formFields.length; index++) {
+
+            var formField = $(formFields[index]);
+            var value = getValue(formField);
+
+            var name = formField.attr('name');
+
+            var rules = validationRules[name];
+
+            for (var key in rules) {
+
+                var rule = rules[key];
+                var validatorFunction = validators[key];
+
+                if (validatorFunction) {
+
+                    var result = validatorFunction(rule, value);
+
+                    if (!result) {
+                        if (!errors[name]) {
+                            errors[name] = [];
+                        }
+
+                        errors[name]['message'] = rule.message;
+
+                    }
+                }
+            }
+
+        }
+
+        var fieldsWithError= [];
+
+        if (Object.keys(errors).length > 0) {
+
+            formID = $(formID);
+
+            for (var error in errors){
+
+                var fieldWithError = formID.find('[name=' + error + ']');
+                fieldsWithError.push(fieldWithError);
+
+            }
+        }
+        else {
+            return true;
+        }
+
+        for (var i = 0; i < fieldsWithError.length; i++){
+            var fieldWithError = fieldsWithError[i];
+
+            var value = getValue(fieldWithError);
+
+            if(result){
+                removeError(fieldWithError);
+                return false;
+            }
+            else{
+                showError(fieldWithError);
+            }
+        }
+
+    };
+
+    function showError(field) {
+        field.addClass('has-error');
     }
 
-    tab1.addEventListener('click', tabs1, false);
-    tab2.addEventListener('click', tabs2, false);
+    function  removeError(field){
+        field.removeClass('has-error');
+    }
 
+    function initialization() {
 
+        var inputsForm2 = $('#evaluateLicence2 .required');
 
-    var state = {
-        currField: null,
-        position: 0,
-        currTab: null,
-        result: [],
-        className: 'hasError'
+        inputsForm2.blur(function(){
+            validate('#evaluateLicence2');
+        });
     }
 
 
+    /*    function onSubmit() {
+
+     if (validate(form)) {
+
+     ajax();
+     }
+
+     return false;
+     }*/
 
 
 
-};
+
+    $(function () {
+
+        initialization();
+    });
+
+
+}(window.jQuery));
