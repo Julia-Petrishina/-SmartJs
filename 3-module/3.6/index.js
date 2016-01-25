f147e4340996a71427fc263ef53f6cd01755e5f1
julia <julia.petrishina@teamdev.com>
2016-01-25 17:50:40 +0200

    Add resolutions

--- 3-module/3.6/index.js
+++ 3-module/3.6/index.js
@@ -1,39 +1,43 @@
 window.onload = function () {
 
-    var request = new XMLHttpRequest();
-    request.open('GET', 'http://jsonplaceholder.typicode.com/users');
-    request.send();
+
     function doSmth(event) {
-        if (request.readyState === request.DONE) {
 
-            var jsonString = event.target.responseText;
-            var object = JSON.parse(jsonString);
-            console.log(object)
+        var request = new XMLHttpRequest();
+        request.open('GET', 'http://jsonplaceholder.typicode.com/users');
+        request.send();
 
-            for (var i = 0; i < object.length; i++) {
-                var currentObject = object[i];
-                var currentName = currentObject.name;
-                var currentEmail = currentObject.email;
+        request.onreadystatechange = function(){
+            if (request.readyState === request.DONE) {
 
-                var list = document.querySelector('.list');
-                var link = document.createElement('a');
+                var jsonString = request.responseText;
+                var object = JSON.parse(jsonString);
 
-                link.innerHTML = currentName;
-                link.setAttribute('href', 'mailto:' + currentEmail);
-                link.onmouseover = function () {
-                    link.setAttribute('title', currentName)
+                for (var i = 0; i < object.length; i++) {
+                    var currentObject = object[i];
+                    var currentName = currentObject.name;
+                    var currentEmail = currentObject.email;
+
+                    var list = document.querySelector('.list');
+                    var link = document.createElement('a');
+
+                    link.innerHTML = currentName;
+                    link.setAttribute('href', 'mailto:' + currentEmail);
+                    link.onmouseover = function () {
+                        link.setAttribute('title', currentName);
+                    };
+                    list.appendChild(link);
                 }
-                list.appendChild(link);
+
             }
-
-            var date = new Date;
-            console.log(event);
         }
-
     };
 
-    request.addEventListener('readystatechange', doSmth);
+    return doSmth();
 
     var btn = document.querySelector('.btn');
-    btn.addEventListener('click', doSmth, false)
+    btn.addEventListener('click', function(){
+        console.log('ytwreyef')
+    }, true);
+
 };
