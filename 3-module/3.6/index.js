476572908b1d455da76aaeb00c44151d90ecafc4
julia petrishina <j.petrishina@pulsarfour.com>
2016-01-27 14:33:54 +0200

    test commit

--- 3-module/3.6/index.js
+++ 3-module/3.6/index.js
@@ -17,7 +17,7 @@ window.onload = function () {
                     var currentObject = object[i];
                     var currentName = currentObject.name;
                     var currentEmail = currentObject.email;
-
+                    console.log('diff');
 
                     var list = document.querySelector('.list');
                     var link = document.createElement('a');
